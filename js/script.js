69d348fa97c638f1865c00b8753a05db376dc97d
lucianpienar <pienarlucianachim@gmail.com>
2023-10-13 01:16:59 +0300

    1st commit 13.10.2023

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 console.log('Hello');
 
+//================================================//
 // Implementing mobile navigation
 const btnNavEl = document.querySelector('.btn-mobile-nav');
 const headerEl = document.querySelector('.header');
@@ -9,7 +10,58 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
+//================================================//
 //Changing the year automatically in the footer
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+//================================================//
+//Smooth scrooling
+
+const allLinksEl = document.querySelectorAll('a:link');
+console.log(allLinksEl);
+allLinksEl.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    const href = link.getAttribute('href');
+
+    //Scroll back to top - the links that have only the #
+    if (href === '#')
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    //scrolling to a certain section - other links
+    if (href.startsWith('#') && href.length > 1) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // close the mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement('div');
+  flex.style.display = 'flex';
+  flex.style.flexDirection = 'column';
+  flex.style.rowGap = '1px';
+
+  flex.appendChild(document.createElement('div'));
+  flex.appendChild(document.createElement('div'));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
+
+// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
